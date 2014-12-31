55ee8807faaa5dba642a814d0fbad42ed14ba810
Brandon Mrsny <bmrsny@gmail.com>
2014-12-31 11:30:00 -0700

    Add Client Registration with status codes

--- lib/traffic_spy/models/source.rb
+++ lib/traffic_spy/models/source.rb
@@ -3,7 +3,7 @@ module TrafficSpy
   class Source
 
     def self.table
-      DB.from(:sources)   #SELECT * FROM articles
+      DB.from(:sources)
     end
 
     def self.create(attributes)
@@ -11,10 +11,17 @@ module TrafficSpy
       :identifier => attributes["identifier"],
       :root_url => attributes["rootUrl"],
       )
-
     end
 
+    def self.exist?(identifier)
+      table.where(
+        :identifier => identifier
+      ).count > 0
 
-
+      #why no working?
+      # table.where(
+      # :identifier => identifier
+      # ).to_a.first[:identifier] == identifier
+    end
   end
 end
