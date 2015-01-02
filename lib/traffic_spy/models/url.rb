3ef6c7f06938a869117e1fef885c5afe34ec0733
Jeffrey Wan <Jwan622@gmail.com>
2015-01-02 13:33:44 -0700

    first statistics url finished

--- lib/traffic_spy/models/url.rb
+++ lib/traffic_spy/models/url.rb
@@ -24,5 +24,20 @@ module TrafficSpy
       ).to_a.first[:id]
     end
 
+    def self.selected_urls(source_id)
+      table.where(
+      :source_id => source_id
+      ).to_a
+    end
+
+    def self.sorted_urls(source_id)
+      list = selected_urls(source_id).map {|url| url[:url]}.inject(Hash.new(0)) {|total, element| total[element] += 1 ; total}
+      list.sort_by {|key, value| value}.reverse
+    end
+
+    # def <=>()
+    #   other.
+    # end
+
   end
 end
